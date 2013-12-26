a29192df6af0b0d8925e3924ad2ea857b40430fa
andrewks777 <andrewks777@yahoo.de>
2013-12-26 20:42:37 +0400

    Update 'AboutMe' plugin

--- addons/languages/Russian/definitions.AboutMe.php
+++ addons/languages/Russian/definitions.AboutMe.php
@@ -4,5 +4,16 @@
 
 // Russian Definitions for the AboutMe plugin.
 
-$definitions["About"] = "Инфо";
-$definitions["Write something about yourself."] = "Заполните информацию о себе";
+$definitions["plugin.AboutMe.about.label"] = "Инфо";
+$definitions["plugin.AboutMe.about.desc"] = "Заполните информацию о себе";
+$definitions["plugin.AboutMe.name.label"] = "Имя";
+$definitions["plugin.AboutMe.sex.label"] = "Пол";
+$definitions["plugin.AboutMe.sex.male"] = "мужской";
+$definitions["plugin.AboutMe.sex.female"] = "женский";
+$definitions["plugin.AboutMe.sex.unknown"] = "не указан";
+$definitions["plugin.AboutMe.birthday.label"] = "Дата рождения";
+$definitions["plugin.AboutMe.birthday.desc"] = "Введите дату в формате ДД/ММ/ГГГГ";
+$definitions["plugin.AboutMe.location.label"] = "Местонахождение";
+$definitions["plugin.AboutMe.email.label"] = "E-mail";
+$definitions["plugin.AboutMe.icq.label"] = "ICQ";
+$definitions["plugin.AboutMe.url.label"] = "URL";
--- addons/plugins/AboutMe/views/about.php
+++ addons/plugins/AboutMe/views/about.php
@@ -6,13 +6,33 @@ if (!defined("IN_ESOTALK")) exit;
 
 $member = $data["member"];
 $about = $data["about"];
+$name = $data["name"];
+$sex = $data["sex"];
+$birthday = $data["birthday"];
+$location = $data["location"];
+$email = $data["email"];
+$icq = $data["icq"];
+$url = $data["url"];
 ?>
 <div id='memberAbout'>
 
-<?php
-	if (ET::$session->user) {
-		echo $about;
-	} else echo T("hidden");
-?>
+<?php if (ET::$session->user): ?>
+
+	<ul class='form'>
+
+		<li><label><?php echo T("plugin.AboutMe.name.label"); ?></label> <div><?php echo $name; ?></div></li>
+		<li><label><?php echo T("plugin.AboutMe.sex.label"); ?></label> <div><?php echo $sex; ?></div></li>
+		<li><label><?php echo T("plugin.AboutMe.birthday.label"); ?></label> <div><?php echo $birthday; ?></div></li>
+		<li><label><?php echo T("plugin.AboutMe.location.label"); ?></label> <div><?php echo $location; ?></div></li>
+		<li><label><?php echo T("plugin.AboutMe.email.label"); ?></label> <div><?php echo "<a href='mailto:".$email."' class='link-email'>".$email."</a>"; ?></div></li>
+		<li><label><?php echo T("plugin.AboutMe.icq.label"); ?></label> <div><?php echo "<img src='http://status.icq.com/online.gif?icq=".$icq."&img=26' style='padding-right:5px'></img><a href='http://www.icq.com/people/".$icq."' rel='nofollow external' target='_blank' class='link-external'>".$icq." </a>"; ?></div></li>
+		<li><label><?php echo T("plugin.AboutMe.url.label"); ?></label> <div><?php echo "<a href='".$url."' rel='nofollow external' target='_blank' class='link-external'>".$url." </a>"; ?></div></li>
+		<li><label><?php echo T("plugin.AboutMe.about.label"); ?></label> <div><?php echo $about; ?></div></li>
+
+	</ul>
+
+<?php else: ?>
+<?php echo T("hidden"); ?>
+<?php endif; ?>
 
 </div>
